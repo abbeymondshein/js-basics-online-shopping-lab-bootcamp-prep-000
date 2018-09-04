10eaafe38bcc6341c394c9f436f0ae6ab02863e3
Abbey Mondshein <abbey.sync@gmail.com>
2018-09-04 03:26:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,57 @@ function setCart(c) {
   return cart;
 }
 
+
+//Use that passed-in string to create a new object representing the item
 function addToCart(item) {
- // write your code here
-}
+let price = Math.floor(Math.random()*100);
+let newItem = {itemName: item, itemPrice: price};
+cart.push(newItem);
+return `${item} has been added to your cart.`
+} 
+
 
 function viewCart() {
   // write your code here
+  var cartContents = [];
+  var inYourCart = "In your cart, you have "
+  
+  if (cart.length===0) {
+    return `Your shopping cart is empty.`
+  }
+  
+  if (cart.length===1) {
+   cartContents.push(`${cart[0].itemName} at $${cart[0].itemPrice}.`)
+   return inYourCart+cartContents;
+  }
+  
+  if (cart.length===2) {
+   cartContents.push(`${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`)
+   return inYourCart+cartContents;
+  }
+  if cart.length>2{
+  for (let i=0;i<cart.length,i++;) {
+        if (i===cart.length-1){
+        cartContents.push(`and ${cart[i].itemName} at $${cart[i].itemPrice}.`)}
+        else {
+          cartContents.push(`${cart[i].itemName} at $${cart[i].itemPrice},`)
+        }
+    return inYourCart+cartContents;
+  }
+}
+  
+  
+  for (let i = 0; i < cart.length; i++) {
+    if (i == cart.length - 1) {
+     text += `and ${cart[i].itemName} at $${cart[i].itemPrice}`
+     } else {
+        text += `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}`
+        }
+    }
+  
+  
+ 
+  
 }
 
 function total() {
